e12981c2ddd082f6c106bc05a367adb564d68cc6
sonyarubakha <sonya.rubakha@gmail.com>
2026-04-19 23:09:49 +0300

    add docstrings

--- Delete Node in a BST/Delete Node in a BST.py
+++ Delete Node in a BST/Delete Node in a BST.py
@@ -1,14 +1,26 @@
 # Definition for a binary tree node.
 from typing import Optional
 class TreeNode:
+    '''
+    a binary tree node class.
+    '''
 
     def __init__(self, val=0, left=None, right=None):
+        '''
+        initializes a binary tree node.
+        '''
         self.val = val
         self.left = left
         self.right = right
 
 class Solution:
+    '''
+    Solution class for task.
+    '''
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        '''
+        deletes the node with the given key in the BST
+        '''
         if not root:
             return
         if key < root.val:
